d1d8c3c0ee93140e79bf34d8c92cd612139450e5
Zhimin Li <zhiminli@visusmac.sci.utah.edu>
2017-01-07 10:19:37 -0700

    2d minkowski

--- js/distance.js
+++ js/distance.js
@@ -1,19 +1,17 @@
-draw_2D_scatter_plot([]);
+var SAMPLESIZE = 500;
+
+$('#minkowski_l').change(minkowski_l_change_event);
 
 function draw_2D_scatter_plot(data){
-
-    data = [[1,2],[2,3],[4,2],[5,4],[1,3],[1,43]];
-
-
-    var margin = {top: 20, right: 20, bottom: 30, left: 40},
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom,
+    var margin  = {top: 20, right: 20, bottom: 20, left: 20},
+        width   = 600 - margin.left - margin.right,
+        height  = 600 - margin.top - margin.bottom,
         xscale  = d3.scale.linear().domain(d3.extent(data, function(d){
             return d[0];
-        })).range([0, width]),
+        })).range([10, width-10]),
         yscale  = d3.scale.linear().domain(d3.extent(data, function(d){
             return d[1];
-        })).range([0, height]);
+        })).range([10, height-10]);
 
     var svg     = d3.select('#canvas').append('svg').attr('width', width).attr('height', height);
         
@@ -26,4 +24,41 @@ function draw_2D_scatter_plot(data){
             return yscale(d[1]);
         })
         .attr('r',5);
+}
+
+
+
+///////////////////////// data generator //////////////////////////////
+function minkowski_2D_Point(l){
+    var points  = [],
+        x       = 0,
+        y       = 0,
+        gap     = 2/SAMPLESIZE;
+        
+
+    for(var i= 0; i <= SAMPLESIZE; i++){
+            x   = -1 + i * gap;
+            temp = Math.abs(Math.pow(x, l));
+            y   = Math.pow(1 - temp, 1.0/l);
+            //y = 1 - temp
+            points.push([x, y]);
+            points.push([x, -y]);
+    }
+    
+    for(var i= 0; i <= SAMPLESIZE; i++){
+            y   = -1 + i * gap;
+            temp = Math.abs(Math.pow(y, l));
+            x   = Math.pow(1 - temp, 1.0/l);
+            //x = 1 - temp
+            console.log(Math.abs(Math.pow(x, l)) + Math.abs(Math.pow(y, l)));
+            points.push([x, y]);
+            points.push([-x, y]);
+    }
+    return points;
+}
+
+function minkowski_l_change_event(){
+    $('#canvas').empty();
+    var data = minkowski_2D_Point($('#minkowski_l').val());
+    draw_2D_scatter_plot(data);
 }
